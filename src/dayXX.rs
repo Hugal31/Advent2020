b64bc77588ab9424e9521d1743f3dabc9e9966c2
Hugo Laloge <hla@lescompanions.com>
2020-12-01 14:07:37 +0100

    Add dayXX.rs template

---
+++ src/dayXX.rs
@@ -0,0 +1,42 @@
+use anyhow::{anyhow, Result};
+
+use crate::Challenge;
+
+pub struct DayXX;
+
+impl Challenge for DayXX {
+    const DAY_NUMBER: u32 = XX;
+
+    type InputType = ();
+    type OutputType = ();
+
+    fn part1(input: &Self::InputType) -> Result<Self::OutputType> {
+        unimplemented!()
+    }
+
+    fn part2(input: &Self::InputType) -> Result<Self::OutputType> {
+        unimplemented!()
+    }
+
+    fn parse(content: &str) -> Result<Self::InputType> {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::DayXX;
+    use crate::Challenge as _;
+
+    #[test]
+    fn test_part1() {
+        unimplemented!()
+    }
+
+    #[test]
+    fn test_solve2() {
+        unimplemented!()
+    }
+}
+
+crate::benchmark_challenge!(crate::dayXX::DayXX);
